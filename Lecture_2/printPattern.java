cec4cf62bfb7ed3c283571ff42e6fd9cfe5b1ed8
Forkyouabhi <forkyouabhi@proton.me>
2025-01-16 10:53:53 -0500

    Lab 2

---
+++ Lecture_2/printPattern.java
@@ -0,0 +1,16 @@
+//
+    1 2 3 4  5  6  7  8  9
+    2 4 6 8  10 12 14 16 18
+    3 6 9 12 15 18 21 24 27
+    4 8 12 16 20 29 28 32 36
+//
+
+
+public class printPattern {
+    public static void  main (String arg[]){
+        for (int i=5; i>=5; i++){
+            for(int i=9;)
+        }
+
+    }
+}

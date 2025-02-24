effbfc3b0c4e1092c8e28992e2255b0cf14cd5c6
Forkyouabhi <forkyouabhi@proton.me>
2025-02-24 17:25:40 -0500

    Brrrrrr

---
+++ Exam_Prep/src/AccountTest.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AccountTest{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String theName= input.nextLine();
+        System.out.println(theName);
+    }
+}
